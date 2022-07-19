936d2949f0a6e7b323147ff31a066cac475a257d
mahan <m.mahmodi1380@gmail.com>
2022-07-19 15:18:25 +0430

    project navbar and route init

--- discord-music-bot-site/src/App.js
+++ discord-music-bot-site/src/App.js
@@ -1,5 +1,37 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AppHeader from './components/AppHeader';
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav> */}
+        <AppHeader />
+
+        {/* A <Routes> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about">{/* <About /> */}</Route>
+          <Route path="/users">{/* <Users /> */}</Route>
+          <Route path="/">{/* <Home /> */}</Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
