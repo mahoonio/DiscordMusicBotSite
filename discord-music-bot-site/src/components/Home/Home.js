0c661391a2a746af6ae8562d8a633eb1bec3745a
mahan <m.mahmodi1380@gmail.com>
2022-07-21 20:22:17 +0430

    first section improvements and second section init.

--- discord-music-bot-site/src/components/Home/Home.js
+++ discord-music-bot-site/src/components/Home/Home.js
@@ -4,7 +4,7 @@ import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
 import sec1pic from '../../assets/sec1pic.png';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
 const Home = () => {
   useEffect(() => {
@@ -24,19 +24,57 @@ const Home = () => {
       <div className={`${styles.section1}`}>
         <div className={`${styles.sec1content}`} data-aos="fade-up">
           <Container>
-            <Row>
+            <Row className="justify-content-center align-items-center">
               <Col xs={12} lg={6}>
                 <img
                   src={sec1pic}
                   alt="web control pannel"
-                  style={{ width: '700px' }}
+                  style={{ width: '800px' }}
                 />
               </Col>
-              <Col xs={{ order: 'first' }} lg={{ order: 'last' }}>
+              <Col
+                style={{ height: '100% !important' }}
+                xs={{ order: 'first' }}
+                lg={{ order: 'last' }}
+              >
                 <div className={`${styles.sectionsdesc}`}>
-                  <h3 style={{ color: 'white' }}>
-                    Easy to use control pannel on web!
-                  </h3>
+                  <h3>Easy to use control pannel on web!</h3>
+                  <p>
+                    With Hydra's extensive web dashboard you're able to
+                    comfortably customize Hydra on the web without having to
+                    remember any commands or syntaxes!
+                  </p>
+                  <Button>Customize now!</Button>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+      <div className={`${styles.section2}`}>
+        <div className={`${styles.sec2content}`} data-aos="fade-up">
+          <Container>
+            <Row className="justify-content-center align-items-center">
+              <Col xs={12} lg={6}>
+                <img
+                  src={sec1pic}
+                  alt="web control pannel"
+                  style={{ width: '800px' }}
+                />
+              </Col>
+              <Col
+                style={{ height: '100% !important' }}
+                xs={{ order: 'first' }}
+                lg={{ order: 'last' }}
+              >
+                <div className={`${styles.sectionsdesc}`}>
+                  <h3>Easy to use control pannel on web!</h3>
+                  <p>
+                    With Hydra's extensive web dashboard you're able to
+                    comfortably customize Hydra on the web without having to
+                    remember any commands or syntaxes!
+                  </p>
+                  <Button>Customize now!</Button>
                 </div>
               </Col>
             </Row>
