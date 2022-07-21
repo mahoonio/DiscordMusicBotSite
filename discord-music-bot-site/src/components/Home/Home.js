bb856f07681e6a31b3c7d76360af1510c6477ef6
mahan <m.mahmodi1380@gmail.com>
2022-07-21 19:16:07 +0430

    first section

--- discord-music-bot-site/src/components/Home/Home.js
+++ discord-music-bot-site/src/components/Home/Home.js
@@ -3,10 +3,12 @@ import styles from './Home.module.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import sec1pic from '../../assets/sec1pic.png';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const Home = () => {
   useEffect(() => {
-    Aos.init();
+    Aos.init({ duration: '600' });
     Aos.refresh();
   }, []);
   return (
@@ -20,7 +22,26 @@ const Home = () => {
         </p>
       </div>
       <div className={`${styles.section1}`}>
-        <div cl7></div>
+        <div className={`${styles.sec1content}`} data-aos="fade-up">
+          <Container>
+            <Row>
+              <Col xs={12} lg={6}>
+                <img
+                  src={sec1pic}
+                  alt="web control pannel"
+                  style={{ width: '700px' }}
+                />
+              </Col>
+              <Col xs={{ order: 'first' }} lg={{ order: 'last' }}>
+                <div className={`${styles.sectionsdesc}`}>
+                  <h3 style={{ color: 'white' }}>
+                    Easy to use control pannel on web!
+                  </h3>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
     </div>
   );
