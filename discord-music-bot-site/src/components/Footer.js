8168f8c0415a431b77adb5c3d3f0ac29e836b430
mahan <m.mahmodi1380@gmail.com>
2022-07-26 19:43:28 +0430

    footer done.

--- discord-music-bot-site/src/components/Footer.js
+++ discord-music-bot-site/src/components/Footer.js
@@ -1,4 +1,5 @@
 import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import styles from './Footer.module.css';
 
 const Footer = () => {
@@ -9,8 +10,35 @@ const Footer = () => {
           <div>
             <h3>Mylo</h3>
             <h5>The Best Music Bot For Discord.</h5>
+            <div className="text-secondary">
+              &copy;Mylo bot 2022 - alright reserved by yazdan zeynal zade and
+              mahan mahmoodi.
+            </div>
           </div>
-          <div className="d-flex justify-content-between align-items-center"></div>
+          <div className="d-flex justify-content-around align-items-center w-100 h-100">
+            <div>
+              <h5>Product</h5>
+              <Link className={styles.footerlink} to={'/commands'}>
+                Commands
+              </Link>
+            </div>
+            <div>
+              <h5>Resources</h5>
+              <Link className={styles.footerlink} to={'/articles'}>
+                Articles
+              </Link>
+            </div>
+            <div>
+              <h5>Business</h5>
+              <a
+                style={{ border: 'none', outline: 'none' }}
+                className={styles.footerlink}
+                href={'mailto:m.mahmodi1380@gmail.com'}
+              >
+                Contact
+              </a>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
