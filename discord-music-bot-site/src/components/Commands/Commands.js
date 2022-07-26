c4b6077e78d6989f591ce0db99226102f74b819a
mahan <m.mahmodi1380@gmail.com>
2022-07-26 20:21:31 +0430

    command request init but doesnt work.

--- discord-music-bot-site/src/components/Commands/Commands.js
+++ discord-music-bot-site/src/components/Commands/Commands.js
@@ -1,8 +1,18 @@
 import styles from './Commands.module.css';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 const Commands = () => {
   const [allCommands, setAllCommands] = useState([]);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios
+      .get('https://mylo-website.herokuapp.com/api/commands/', {})
+      .then((res) => {
+        console.log('res: ', res);
+      })
+      .catch((err) => {
+        console.error('error: ', err);
+      });
+  }, []);
   return <div className={`${styles.commandscnt}`}></div>;
 };
 
