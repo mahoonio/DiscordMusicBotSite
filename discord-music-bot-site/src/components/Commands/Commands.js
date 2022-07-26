ec2c8a7672713371f4876b738085ee94da6400f7
mahan <m.mahmodi1380@gmail.com>
2022-07-26 19:54:35 +0430

    command init

--- discord-music-bot-site/src/components/Commands/Commands.js
+++ discord-music-bot-site/src/components/Commands/Commands.js
@@ -1,5 +1,8 @@
 import styles from './Commands.module.css';
+import { useState, useEffect } from 'react';
 const Commands = () => {
+  const [allCommands, setAllCommands] = useState([]);
+  useEffect(() => {}, []);
   return <div className={`${styles.commandscnt}`}></div>;
 };
 
