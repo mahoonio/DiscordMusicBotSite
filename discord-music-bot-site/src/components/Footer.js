7c7916bbbd9e211db2538a06b2302d48071cec9f
mahan <m.mahmodi1380@gmail.com>
2022-07-26 16:24:02 +0430

    footer edit

--- discord-music-bot-site/src/components/Footer.js
+++ discord-music-bot-site/src/components/Footer.js
@@ -1,7 +1,20 @@
+import { Container } from 'react-bootstrap';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <div className={styles.appfootermaincontainer}>hello</div>;
+  return (
+    <div className={styles.appfootermaincontainer}>
+      <Container>
+        <div className="d-flex justify-content-between h-100">
+          <div>
+            <h3>Mylo</h3>
+            <h5>The Best Music Bot For Discord.</h5>
+          </div>
+          <div className="d-flex justify-content-between align-items-center"></div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Footer;
