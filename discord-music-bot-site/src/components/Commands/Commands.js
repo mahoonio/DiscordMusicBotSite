e8a27782f22d4188e4a52fcc71586c81b4272fa9
mahan <m.mahmodi1380@gmail.com>
2022-07-27 18:00:29 +0430

    command req mock

--- discord-music-bot-site/src/components/Commands/Commands.js
+++ discord-music-bot-site/src/components/Commands/Commands.js
@@ -1,19 +1,29 @@
 import styles from './Commands.module.css';
 import { useState, useEffect } from 'react';
+import commandMock from '../../mock/commandMock';
 import axios from 'axios';
 const Commands = () => {
   const [allCommands, setAllCommands] = useState([]);
   useEffect(() => {
     axios
-      .get('https://mylo-website.herokuapp.com/api/commands/', {})
+      .get('https://mylo-website.herokuapp.com/api/commands/', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
       .then((res) => {
         console.log('res: ', res);
       })
       .catch((err) => {
         console.error('error: ', err);
       });
+    setAllCommands(commandMock);
   }, []);
-  return <div className={`${styles.commandscnt}`}></div>;
+  return (
+    <div className={`${styles.commandscnt}`}>
+      {commandMock && <div style={{ color: 'white' }}>hello</div>}
+    </div>
+  );
 };
 
 export default Commands;
