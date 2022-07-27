1ba1add7e942baf1cba4f3620ba5db67a53f64bf
mahan <m.mahmodi1380@gmail.com>
2022-07-27 18:19:29 +0430

    command req done and page structure.

--- discord-music-bot-site/src/components/Commands/Commands.js
+++ discord-music-bot-site/src/components/Commands/Commands.js
@@ -1,5 +1,6 @@
 import styles from './Commands.module.css';
 import { useState, useEffect } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import commandMock from '../../mock/commandMock';
 import axios from 'axios';
 const Commands = () => {
@@ -12,16 +13,28 @@ const Commands = () => {
         },
       })
       .then((res) => {
-        console.log('res: ', res);
+        setAllCommands(res.data);
       })
       .catch((err) => {
-        console.error('error: ', err);
+        alert(
+          'there was a problem connecting to the server. \nif you are in iran,use VPN'
+        );
       });
     setAllCommands(commandMock);
+    return;
   }, []);
   return (
     <div className={`${styles.commandscnt}`}>
-      {commandMock && <div style={{ color: 'white' }}>hello</div>}
+      {commandMock && (
+        <Container className="h-100">
+          <Row className="h-100">
+            <Col>
+              <div className={`${styles.filterscnt}`}></div>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+      )}
     </div>
   );
 };
