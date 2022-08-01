ffbf6723cd175daea7135af22cdd394d1a410f73
mahan <m.mahmodi1380@gmail.com>
2022-08-01 16:02:03 +0430

    article page init.

--- discord-music-bot-site/src/components/Articles/Articles.js
+++ discord-music-bot-site/src/components/Articles/Articles.js
@@ -1,7 +1,8 @@
 import AppHeader from '../AppHeader';
+import styles from './Articles.module.css';
 
 const Articles = () => {
-  return <>{/* <AppHeader /> */}</>;
+  return <div className={styles.articlescnt}></div>;
 };
 
 export default Articles;
