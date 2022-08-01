bb688b097af31dd74f23c4db09631cc9265ddf45
mahan <m.mahmodi1380@gmail.com>
2022-08-01 18:36:29 +0430

    articles req done

--- discord-music-bot-site/src/components/Articles/Articles.js
+++ discord-music-bot-site/src/components/Articles/Articles.js
@@ -1,7 +1,40 @@
 import AppHeader from '../AppHeader';
 import styles from './Articles.module.css';
+import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 
 const Articles = () => {
+  const [allArticles, setAllArticles] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://mylo-website.herokuapp.com/api/articles/', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        setAllArticles(res.data);
+        console.log('articles : ', res.data);
+      })
+      .catch((err) => {
+        toast.error(
+          'there was a problem connecting to the server.if you have internet restrictions please use VPN',
+          {
+            position: 'top-center',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          }
+        );
+      });
+  }, []);
+
   return <div className={styles.articlescnt}></div>;
 };
 
